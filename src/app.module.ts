bb495cf520a9d9ae8fa8657a27dffab7e7b6499a
Алексей <101291963+Alexeykoh@users.noreply.github.com>
2025-02-09 11:41:18 +0300

    feat: Add Docker support with development and production configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,56 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './api/users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
 import { JwtStrategy } from './auth/guards/jwt.strategy';
-import { ConfigModule } from '@nestjs/config';
 import { TokenModule } from './token/token.module';
 import { ClientsModule } from './api/clients/clients.module';
 import { ProjectsModule } from './api/projects/projects.module';
 import { TasksModule } from './api/tasks/tasks.module';
 import { TimeLogsModule } from './api/time_logs/time_logs.module';
 import { CurrenciesModule } from './api/currencies/currencies.module';
-import * as process from 'node:process';
 import { TagsModule } from './api/tags/tags.module';
 import { SearchModule } from './api/search/search.module';
 import { TaskSharedModule } from './api/task-members/task-shared.module';
 import { ProjectSharedModule } from './api/project-shared/project-shared.module';
 import { GuardsModule } from './guards/guards.module';
 import { NotesModule } from './api/notes/notes.module';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT as unknown as number,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env' : '.env.dev',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): PostgresConnectionOptions => {
+        const dbConfig: PostgresConnectionOptions = {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          // autoLoadEntities: true,
+          synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        };
+
+        console.log('Database connection config:', {
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.username,
+          database: dbConfig.database,
+          nodeEnv: configService.get('NODE_ENV'),
+        });
+
+        return dbConfig;
+      },
+      inject: [ConfigService],
     }),
     UsersModule,
     AuthModule,
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as process from 'node:process';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
   logger.log('Application is starting...');
+
+  const configService = app.get(ConfigService);
+  const environment = configService.get('NODE_ENV') || 'development';
 
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
@@ -18,9 +22,15 @@ async function bootstrap() {
     origin: '*', // This allows all domains to access your resources
   });
 
+  const environmentInfo = {
+    environment: environment.toUpperCase(),
+    databaseHost: configService.get('DB_HOST'),
+    // Add other relevant information, but be careful with sensitive data
+  };
+
   const config = new DocumentBuilder()
     .setTitle('API')
-    .setDescription('API description')
+    .setDescription(`API description (Environment Info: ${JSON.stringify(environmentInfo)})`)
     .setVersion('1.0')
     .addBearerAuth()
     .build();
@@ -55,20 +65,13 @@ async function bootstrap() {
     },
   });
 
-  const currencyConfig = new DocumentBuilder()
-    .setTitle('Currency API')
-    .setDescription('API documentation for currency management')
-    .setVersion('1.0')
-    .build();
-  const currencyDocument = SwaggerModule.createDocument(app, currencyConfig);
-  SwaggerModule.setup('currency-api', app, currencyDocument);
-
   const port = process.env.PORT || 3000; // Use the port from the environment variable or default to 3000
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   // Log the host and port information
   const host = process.env.HOST || 'localhost'; // Default to localhost if HOST is not set
   console.log(`Server is running on http://${host}:${port}`);
+  console.log(`Environment: ${environment.toUpperCase()}`);
 }
 
 bootstrap();
