bcd2dffb0958853bd9fba2ce9a4c971633c93cbd
Алексей <101291963+Alexeykoh@users.noreply.github.com>
2025-05-17 20:21:09 +0300

    test: ci-cd

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { TransformInterceptor } from './interceptors/transform.interceptor';
 import { Logger, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
+// hello ci-cd
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new (await import('@nestjs/common')).ValidationPipe({ transform: true }));
