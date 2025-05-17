d73844315e46b12d9d15f8cbc61d74158311cabc
Алексей <101291963+Alexeykoh@users.noreply.github.com>
2025-05-17 20:25:46 +0300

    test: ci-cd

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { TransformInterceptor } from './interceptors/transform.interceptor';
 import { Logger, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
-// hello ci-cd
+// hello ci-cd v2
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
